5a101295c1ad918d32c784fd8bf7a4edb9ebd962
wudiming <sxwudiming@163.com>
2025-07-01 21:28:09 +0800

    Update recommon.py

--- python/recommon.py
+++ python/recommon.py
@@ -1,8 +1,11 @@
 with open('common.rs', 'r') as f:
-              content = f.read()
-          content = content.replace(
-              '"https://admin.rustdesk.com".to_owned()',
-              '"https://rd.1128.pp.ua:21114".to_owned()'
-          )
-          with open('common.rs', 'w') as f:
-              f.write(content)
+    content = f.read()
+
+content = content.replace(
+    '"https://admin.rustdesk.com".to_owned()',
+    '"https://rd.1128.pp.ua:21114".to_owned()'
+)
+
+with open('common.rs', 'w') as f:
+    f.write(content)
+
