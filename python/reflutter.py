c3b7a3a03760a26432fae325e54a0869777f366f
wudiming <sxwudiming@163.com>
2025-07-01 21:06:02 +0800

    Create reflutter.py

---
+++ python/reflutter.py
@@ -0,0 +1,8 @@
+with open('flutter-ci.yml', 'r') as f:
+              content = f.read()
+          content = content.replace(
+              'upload-artifact: false',
+              'upload-artifact: true'
+          )
+          with open('flutter-ci.yml', 'w') as f:
+              f.write(content)
