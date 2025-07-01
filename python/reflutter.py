970c6bda594c31f4a1d20ce738f749752fb9451c
wudiming <sxwudiming@163.com>
2025-07-01 21:31:24 +0800

    Update reflutter.py

--- python/reflutter.py
+++ python/reflutter.py
@@ -1,8 +1,10 @@
 with open('flutter-ci.yml', 'r') as f:
-              content = f.read()
-          content = content.replace(
-              'upload-artifact: false',
-              'upload-artifact: true'
-          )
-          with open('flutter-ci.yml', 'w') as f:
-              f.write(content)
+    content = f.read()
+
+content = content.replace(
+    'upload-artifact: false',
+    'upload-artifact: true'
+)
+
+with open('flutter-ci.yml', 'w') as f:
+    f.write(content)
