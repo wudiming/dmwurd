1187917121f857b5641c02b76b6a50ca0809b2c0
wudiming <sxwudiming@163.com>
2025-07-01 21:05:06 +0800

    Create recommos.py

---
+++ python/recommos.py
@@ -0,0 +1,8 @@
+with open('common.rs', 'r') as f:
+              content = f.read()
+          content = content.replace(
+              '"https://admin.rustdesk.com".to_owned()',
+              '"https://rd.1128.pp.ua:21114".to_owned()'
+          )
+          with open('common.rs', 'w') as f:
+              f.write(content)
