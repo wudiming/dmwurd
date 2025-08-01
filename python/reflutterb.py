bdba02eb2f39f28409c734ebb9bf023dd111ee12
wudiming <sxwudiming@163.com>
2025-08-01 13:59:46 +0800

    Update reflutterb.py

--- python/reflutterb.py
+++ python/reflutterb.py
@@ -5,7 +5,7 @@ tag = os.environ.get("remote_tag")
 if not tag:
     raise ValueError("环境变量 remote_tag 未设置")
 
-file_path = ".github/workflows/flutter-build.yml"
+file_path = "flutter-build.yml"
 
 with open(file_path, "r", encoding="utf-8") as f:
     content = f.read()
