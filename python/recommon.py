c0be0a32fada4f6b0a513e97e72c9bbcd619e5e6
wudiming <sxwudiming@163.com>
2025-08-01 13:53:34 +0800

    Update recommon.py

--- python/recommon.py
+++ python/recommon.py
@@ -1,6 +1,16 @@
 with open('common.rs', 'r') as f:
     content = f.read()
 
+old_line = "let response_url = resp.url;"
+new_lines = (
+    "    let mut response_url = resp.url;\n"
+    "    response_url = response_url.replace(\n"
+    "        \"github.com/rustdesk/rustdesk\",\n"
+    "        \"github.com/wudiming/dmwurd\"\n"
+    "    );"
+)
+content = content.replace(old_line, new_lines)
+
 content = content.replace(
     '"https://admin.rustdesk.com".to_owned()',
     '"https://rd.1128.pp.ua:21114".to_owned()'
