1a3d6077d30ccba324be594c8e8741fad85ffa18
wudiming <sxwudiming@163.com>
2025-09-15 14:39:35 +0800

    Update reflutterc.py

--- python/reflutterc.py
+++ python/reflutterc.py
@@ -1,4 +1,10 @@
-with open('flutter-ci.yml', 'r') as f:
+import sys # 导入 sys 模块
+
+# 从命令行参数获取文件路径
+file_path = sys.argv[1]
+
+# 使用传入的文件路径，并统一加上 utf-8 编码
+with open(file_path, 'r', encoding='utf-8') as f:
     content = f.read()
 
 content = content.replace(
@@ -6,5 +12,7 @@ content = content.replace(
     'upload-artifact: true'
 )
 
-with open('flutter-ci.yml', 'w') as f:
+with open(file_path, 'w', encoding='utf-8') as f:
     f.write(content)
+
+print(f"✅ 文件 '{file_path}' 中 upload-artifact 已修改为: true")
